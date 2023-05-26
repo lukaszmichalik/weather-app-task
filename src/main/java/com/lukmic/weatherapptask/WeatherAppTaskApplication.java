d46f67b164ee996e61fc2aa2fdda9e14b5a01d18
lukasz.michalik <lukaszmichalik98@gmail.com>
2023-05-26 13:23:07 +0200

    Project initialization

---
+++ src/main/java/com/lukmic/weatherapptask/WeatherAppTaskApplication.java
@@ -0,0 +1,13 @@
+package com.lukmic.weatherapptask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAppTaskApplication.class, args);
+    }
+
+}
